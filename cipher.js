d5047958a54f7d0933cdeaa511ab31d03d22f4ef
zotacy <tacyzacharyo@johndeere.com>
2020-09-30 12:02:44 -0400

    Added style.css

--- cipher.js
+++ cipher.js
@@ -1,6 +1,6 @@
 //variables
 let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-let word = 'Zachary';
+let word;
 let codeWord;
 //Function to shuffle array
 function shuffleArray(array) {
